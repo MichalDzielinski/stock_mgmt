ce625da3c9173f13b979d17ebaef7ba40513ebc5
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-12 12:01:12 +0200

    List items page

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from stock import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('list/', views.list_items, name='list'),
     path('admin/', admin.site.urls),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .forms import StockCreateForm
+from .models import Stock
 
 def home(request):
     title = 'Hello to my page'
     form = StockCreateForm()
     context = {'title': title, 'form': form}
     return render(request, 'home.html', context)
+
+def list_items(request):
+    title = 'List'
+    qs = Stock.objects.all()
+    context = {'title': title, 'qs': qs}
+    return render(request, 'list.html', context)
