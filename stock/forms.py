6e7ab8ada5cb02eb855fd5dd679e835a3805c22a
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-14 08:53:38 +0200

    Two fields validation [can't be empty] in StockCreateForm

--- stock/forms.py
+++ stock/forms.py
@@ -6,6 +6,19 @@ class StockCreateForm(forms.ModelForm):
         model = Stock
         fields = ['category', 'item_name', 'quantity']
 
+    def clean_category(self):
+        category = self.cleaned_data.get('category')
+        if not category:
+            raise forms.ValidationError('This field is definitely required')
+        return category
+    
+    def clean_item_name(self):
+        item_name = self.cleaned_data.get('item_name')
+        if not item_name:
+            raise forms.ValidationError('This field is definitely required')
+        return item_name
+
+
 class StockSearchForm(forms.ModelForm):
     class Meta:
         model = Stock
