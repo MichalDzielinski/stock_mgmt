a9284e3dcc8b50703eda0cfb51fa038ee60278ad
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-15 11:06:05 +0200

    Postgres Triggers part 1

--- stock/models.py
+++ stock/models.py
@@ -24,11 +24,27 @@ class Stock(models.Model):
     reorder_level = models.IntegerField(default=0, null=True, blank=True)
     last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    date = models.DateTimeField(auto_now_add=False, auto_now=False)
+    
 
     # export_to_CSV = models.BooleanField(default=False)
 
     def __str__(self):
         return self.item_name +" "+str(self.quantity)
 
+class StockHistory(models.Model):
+    id = models.IntegerField(primary_key=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    item_name = models.CharField(max_length=50, blank=True, null=True)
+    quantity = models.IntegerField(default=0, blank=True, null=True)
+    receive_quantity = models.IntegerField(default=0, blank=True, null=True)
+    receive_by = models.CharField(max_length=50, blank=True, null=True)
+    issue_quantity = models.IntegerField(default=0, null=True, blank=True)
+    issue_by = models.CharField(max_length=50, blank=True, null=True)
+    issue_to = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=50, blank=True, null=True)
+    created_by = models.CharField(max_length=50, blank=True, null=True)
+    reorder_level = models.IntegerField(default=0, null=True, blank=True)
+    last_updated = models.DateTimeField(auto_now_add=False, auto_now=False, primary_key=True)
+    timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
+    
 
